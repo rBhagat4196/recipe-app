1e719b9eb26cd5c25359260a2e095f9d9a0cb7ab
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-05-24 14:42:46 +0530

    backend integration/ api integration

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api', for local hos
+  // for deployment
+  baseURL : 'https://recipe-app-1-f5lh.onrender.com/api/'   
 });
 
 export default api;
